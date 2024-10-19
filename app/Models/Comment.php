ff650e1616c43b217eadb9bb0441cc9e949bcff1
Lukasz Markiewicz <markiewicz.lukasz98@gmail.com>
2024-10-19 12:47:38 +0200

    add Models

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'comment',
+        'user_id',
+        'post_id',
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
